3cb614d818d78350e715ddbcbadf6c9a850c3725
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2012-02-28 12:25:36 +0000

    "Streamlined initializing, and added replaceText plugin."

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,23 +1,48 @@
 (function(){
+    
+    var initialize = function() {
+        load_jq();
+    };
 
-	// the minimum version of jQuery we want
-    var v = "1.7.1";
-
-	// check prior inclusion and version
-	if ( window.jQuery === undefined || window.jQuery.fn.jquery < v ) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if ( !done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") ) {
-				done = true;
-				bookmarklet();
-			}
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-	} else {
-		bookmarklet();
-	}
+    //load jQuery
+    var load_jq = function() {
+        // the minimum version of jQuery we want
+        var v = "1.7.1";
+        // check prior inclusion and version
+        if ( window.jQuery === undefined || window.jQuery.fn.jquery < v ) {
+            var done_jq = false;
+            var script_jq = document.createElement("script");
+            script_jq.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+            script_jq.onload = script_jq.onreadystatechange = function(){
+                if ( !done_jq && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") ) {
+                    done_jq = true;
+                    load_rt();
+                }
+            };
+            document.getElementsByTagName("head")[0].appendChild(script_jq);
+        } else {
+            load_rt();
+        }
+    };
+    
+    // load replaceText
+    var load_rt = function() {
+        // check and include replaceText plugin
+        if ( window.jQuery().replaceText === undefined) {
+            var done_rt = false;
+            var script_rt = document.createElement("script");
+            script_rt.src = "https://raw.github.com/cowboy/jquery-replacetext/master/jquery.ba-replacetext.min.js";
+            script_rt.onload = script_rt.onreadystatechange = function(){
+                if ( !done_rt && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") ) {
+                    done_rt = true;
+                    bookmarklet();
+                }
+            };
+            document.getElementsByTagName("head")[0].appendChild(script_rt);
+        } else {
+            bookmarklet();
+        }
+    };
 
     // initialize bookmarklet
 	function bookmarklet() {
@@ -75,5 +100,7 @@
             
         })(jQuery);
     }
+    
+    initialize();
 
 })();
