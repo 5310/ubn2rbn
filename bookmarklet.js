4a70efc8eb813480fae67088a7c2bf090ee9b9a0
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2012-02-29 16:39:15 +0000

    "Branched and recommitted start of selection code."

--- bookmarklet.js
+++ bookmarklet.js
@@ -35,17 +35,36 @@
             script_rt.onload = script_rt.onreadystatechange = function(){
                 if ( !done_rt && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") ) {
                     done_rt = true;
-                    bookmarklet();
+                    load_ws();
                 }
             };
             document.getElementsByTagName("head")[0].appendChild(script_rt);
         } else {
+            load_ws();
+        }
+    };
+    
+    // load wrapSelection
+    var load_ws = function() {
+        // check and include wrapSelection plugin
+        if ( window.jQuery().wrapSelection === undefined) {
+            var done_ws = false;
+            var script_ws = document.createElement("script");
+            script_ws.src = "https://raw.github.com/tzicatl/jquery.wrapSelection/master/jquery.wrapSelection.js";
+            script_ws.onload = script_ws.onreadystatechange = function(){
+                if ( !done_ws && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") ) {
+                    done_ws = true;
+                    bookmarklet();
+                }
+            };
+            document.getElementsByTagName("head")[0].appendChild(script_ws);
+        } else {
             bookmarklet();
         }
     };
-   
+
     // initialize bookmarklet
-	function bookmarklet() {
+    function bookmarklet() {
         (window.bookmarklet = function(jQuery) {
             
             var $ = jQuery;
