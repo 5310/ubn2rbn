0d1d0cb1c77b104213f7e1576c408db183e12d43
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2012-03-25 17:20:39 +0530

    .

--- bookmarklet.js
+++ bookmarklet.js
@@ -178,7 +178,8 @@
             while ((match = re.exec(rawtext)) != null) {
 	        rawtext = rawtext.slice(0, match.index+2) + "্" + rawtext.slice(match.index+2);
             }
-            text.html(rawtext);
+            console.log(rawtext);
+            //text.html(rawtext);
             
             // do the astral plane!
             var u;
